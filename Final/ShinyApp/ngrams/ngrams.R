ba56ecd3a8af5608e5d54ea8dea3133b21a470ba
MBARicardoOssio <71062763+MBARicardoOssio@users.noreply.github.com>
2020-12-13 20:48:37 -0600

    Add files via upload

--- Final/ShinyApp/ngrams/ngrams.R
+++ Final/ShinyApp/ngrams/ngrams.R
@@ -1,1 +1,19 @@
+#
+# Coursera Data Science Capstone - Final Project
+# Ricardo A Garcia Osio, December 07, 2020
+#
+
+# n-gram files
+
+#bigrams
+bigrams <- readRDS("C:/Users/TOSHIBA/Documents/Coursera/10 Data Science Capstone/Capstone-master/Final/ShinyApplication/ngrams/bigram.RData")
+head(bigrams,10)
+
+#trigrams
+trigrams <- readRDS("C:/Users/TOSHIBA/Documents/Coursera/10 Data Science Capstone/Capstone-master/Final/ShinyApplication/ngrams/trigram.RData")
+head(trigrams,10)
+
+#quadgrams
+quadgrams <- readRDS("C:/Users/TOSHIBA/Documents/Coursera/10 Data Science Capstone/Capstone-master/Final/ShinyApplication/ngrams/quadgram.RData")
+head(quadgrams,10)
 
